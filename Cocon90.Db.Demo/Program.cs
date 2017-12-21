b9dc0f25033abe791928494ee1cd351713ec7f76
xingzhu.song <xingzhu.song@yungoal.com>
2017-12-21 14:34:37 +0800

    commit.

--- Cocon90.Db.Demo/Program.cs
+++ Cocon90.Db.Demo/Program.cs
@@ -15,7 +15,7 @@ namespace Cocon90.Db.Demo
 
             var dh = Cocon90.Db.Common.Db.GetDataHelper("Sqlite", "Data Source=${app}\\test.db3");
             //var dh = Cocon90.Db.Common.Db.GetDataHelper(DbTypeEnum.Mysql,"server=127.0.0.1;port=3306;database=datarepair;uid=root;pwd=123456;");
-            //var dh = Cocon90.Db.Common.Db.GetDataHelper(DbTypeEnum.SqlServer, "server=dataanalyze.database.chinacloudapi.cn;database=cy-data;uid=dataanalyze;pwd=password001!;");
+            //var dh = Cocon90.Db.Common.Db.GetDataHelper(DbTypeEnum.SqlServer, "server=.;database=cy-data;uid=sa;pwd=password001!;");
             var updsql = dh.GetUpdateSql(new Model.CountryLanguageModel { Code = 23, CodeAndLang = "bbb", Date = DateTime.Now, Percent = 2 }, true, "IsOfficial=1");
             dh.CreateOrUpdateTable<Model.CountryLanguageModel>();
             var sqlExist = dh.GetInsertIfNotExistPrimeryKeySql<Model.CountryLanguageModel>(new Model.CountryLanguageModel { CodeAndLang = "asdf", Date = DateTime.Now, Code = 099, Percent = 3 }, 23, "Lang");
---
+++ Cocon90.Db.XUnitTest/AutoGenColumnTest.cs
@@ -0,0 +1,23 @@
+﻿using Cocon90.Db.Common;
+using Cocon90.Db.Common.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocon90.Db.XUnitTest
+{
+    [TestClass]
+    public class AutoGenColumnTest
+    {
+
+        [TestMethod]
+        public void TestInsert()
+        {
+            var dh = Cocon90.Db.Common.Db.GetDataHelper(DbTypeEnum.SqlServer, "server=.;database=Test;uid=sa;pwd=123456;");
+
+        }
+    }
+}
